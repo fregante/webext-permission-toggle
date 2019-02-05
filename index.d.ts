7ceccc39a956a1e3b414bff516cc4feb927cadad
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 19:49:30 -0500

    Added types for the package.

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module 'webext-domain-permission-toggle' {
+	export interface Options {
+		title?: string;
+		reloadOnSuccess?: string;
+	}
+
+	export function addContextMenu(options: Options): void;
+}
