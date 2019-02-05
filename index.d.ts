46d0c2316a187d3d5d7902cbb5cc51d199ca1899
Nick Taylor <nick@iamdeveloper.com>
2019-02-04 22:40:48 -0500

    Removed module declaration in types.

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,21 @@
-declare module 'webext-domain-permission-toggle' {
-	export interface Options {
-		/**
-		 * The title of the action in the context menu.
-		 */
-		title?: string;
-
-		/**
-		 * If the user accepts the new permission, they will be asked to reload the current tab.
-		 * Set a `string` to customize the message and `false` to avoid the reload and its request.
-		 */
-		reloadOnSuccess?: string|boolean;
-	}
+export interface Options {
+	/**
+	 * The title of the action in the context menu.
+	 */
+	title?: string;
 
 	/**
-	 * Adds an item to the browser action icon's context menu.
-	 * The user can access this menu by right clicking the icon. If your extension doesn't have any action or
-	 * popup assigned to the icon, it will also appear with a left click.
-	 *
-	 * @param options {Options}
+	 * If the user accepts the new permission, they will be asked to reload the current tab.
+	 * Set a `string` to customize the message and `false` to avoid the reload and its request.
 	 */
-	export function addContextMenu(options?: Options): void;
+	reloadOnSuccess?: string|boolean;
 }
+
+/**
+ * Adds an item to the browser action icon's context menu.
+ * The user can access this menu by right clicking the icon. If your extension doesn't have any action or
+ * popup assigned to the icon, it will also appear with a left click.
+ *
+ * @param options {Options}
+ */
+export function addContextMenu(options?: Options): void;
