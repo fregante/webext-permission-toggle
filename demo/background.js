3f961f5c2988e7050fc5a8930f9f91ec8ccb687a
Federico Brigante <me@fregante.com>
2024-01-16 16:07:02 +0800

    Improve demo extension

--- demo/background.js
+++ demo/background.js
@@ -1,3 +1,19 @@
 import addDomainPermissionToggle from '..';
 
 addDomainPermissionToggle();
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (!tab.url) {
+		console.log('No access to tab', tabId);
+		return;
+	}
+
+	console.log('Access to tab', tabId, tab.url);
+	chrome.scripting.executeScript({
+		target: {tabId},
+		function() {
+			document.body.style.backgroundColor = 'yellow';
+			console.log('chrome.tabs.onUpdated was fired');
+		},
+	});
+});
