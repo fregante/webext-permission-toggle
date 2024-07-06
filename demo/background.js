6a0f21c9971a5c507aaf7d644dc412ceb1e18ef8
Federico Brigante <me@fregante.com>
2024-07-06 15:32:51 +0800

    Remove a broader permission if granted (#56)

--- demo/background.js
+++ demo/background.js
@@ -14,3 +14,8 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 	const why = await chromeP.permissions.contains({origins: [tab.url]}) ? 'granted' : 'just because of activeTab';
 	console.log('Access to tab', tabId, tab.url, why);
 });
+
+(chrome.action ?? chrome.browserAction).onClicked.addListener(async () => chromeP.permissions.request({origins: ['*://*/*']}));
+
+chrome.permissions.onAdded.addListener(permissions => console.log('Permissions added:', permissions));
+chrome.permissions.onRemoved.addListener(permissions => console.log('Permissions removed:', permissions));
